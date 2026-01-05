0a94d2d1343eb9e6a966de51042112729c750cf3
quocanhle0608-art <quocanhle0608@gmail.com>
2026-01-05 18:42:19 +0700

    Add files via upload

---
+++ BaiTap2.java
@@ -0,0 +1,43 @@
+package quocanh;
+import java.util.Scanner;
+
+public class BaiTap2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String hoVaTen;
+        String studentID;
+        String hometown;
+        float gpa;
+        String major;
+        boolean scholarship;
+
+        System.out.print("Nhap ho va ten: ");
+        hoVaTen = sc.nextLine();
+
+        System.out.print("Nhap ma sinh vien: ");
+        studentID = sc.nextLine();
+        sc.nextLine();
+
+        System.out.print("Nhap que quan: ");
+        hometown = sc.nextLine();
+
+        System.out.print("Nhap GPA: ");
+        gpa = sc.nextFloat();
+        sc.nextLine();
+
+        System.out.print("Nhap chuyen nganh: ");
+        major = sc.nextLine();
+
+        System.out.print("Co hoc bong khong? (true/false): ");
+        scholarship = sc.nextBoolean();
+
+        System.out.println("\n--- THONG TIN SINH VIEN ---");
+        System.out.println("Ho va ten: " + hoVaTen);
+        System.out.println("Ma sinh vien: " + studentID);
+        System.out.println("Que quan: " + hometown);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Chuyen nganh: " + major);
+        System.out.println("Hoc bong: " + scholarship);
+    }
+}
