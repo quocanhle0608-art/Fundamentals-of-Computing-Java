36bd9e7dadc0022fe6c2bfa3da0e16fbfef86ef1
quocanhle0608-art <quocanhle0608@gmail.com>
2026-01-07 22:49:49 +0700

    BaiTap2.java

--- BaiTap2.java
+++ BaiTap2.java
@@ -1,5 +1,7 @@
-package quocanh;
+package quocanh ;
+
 import java.util.Scanner;
+import java.time.LocalDate;
 
 public class BaiTap2 {
     public static void main(String[] args) {
@@ -9,25 +11,28 @@ public class BaiTap2 {
         String studentID;
         String hometown;
         float gpa;
-        String mayjor;
+        String major;
         boolean scholarship;
+        LocalDate dayOfBirth;
 
         System.out.print("Nhap ho va ten: ");
         hoVaTen = sc.nextLine();
 
         System.out.print("Nhap ma sinh vien: ");
         studentID = sc.nextLine();
-        sc.nextLine();
 
         System.out.print("Nhap que quan: ");
         hometown = sc.nextLine();
+
+        System.out.print("Nhap ngay sinh (YYYY-MM-DD): ");
+        dayOfBirth = LocalDate.parse(sc.nextLine());
 
         System.out.print("Nhap GPA: ");
         gpa = sc.nextFloat();
         sc.nextLine();
 
         System.out.print("Nhap chuyen nganh: ");
-        mayjor = sc.nextLine();
+        major = sc.nextLine();
 
         System.out.print("Co hoc bong khong? (true/false): ");
         scholarship = sc.nextBoolean();
@@ -36,9 +41,11 @@ public class BaiTap2 {
         System.out.println("Ho va ten: " + hoVaTen);
         System.out.println("Ma sinh vien: " + studentID);
         System.out.println("Que quan: " + hometown);
+        System.out.println("Ngay sinh: " + dayOfBirth);
         System.out.println("GPA: " + gpa);
-        System.out.println("Chuyen nganh: " + mayjor);
+        System.out.println("Chuyen nganh: " + major);
         System.out.println("Hoc bong: " + scholarship);
+
+        sc.close();
     }
 }
-
